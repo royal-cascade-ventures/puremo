6cb34ce6e06a8d3ed45f3cb34ef6e2732744187a
atreyasinha <atreyavis@gmail.com>
2025-04-30 23:59:52 -0700

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="app-container">
       <div className="card">
-        <h1>Welcome to React with Docker!</h1>
-        <p>This is a simple React app running inside a Docker container.</p>
+        <h1>Welcome to puremo!</h1>
+        <p>This is a simple React app.</p>
         <button type="button" onClick={() => console.log('Button clicked!')}>
           Click Me
         </button>
