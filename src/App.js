b757b47faf75cef0b206b93eda3205f904790b17
atreyasinha <atreyavis@gmail.com>
2025-04-30 19:05:13 -0700

    add react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="card">
+        <h1>Welcome to React with Docker!</h1>
+        <p>This is a simple React app running inside a Docker container.</p>
+        <button onClick={() => alert('Button clicked!')}>Click Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
