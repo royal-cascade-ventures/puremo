61e369bf94662fddc0f28e4d6d1186b22f89f78d
atreyasinha <atreyavis@gmail.com>
2025-04-30 20:33:30 -0700

    check auto-deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app-container">
       <div className="card">
-        <h1>Welcome to React with Docker!</h1>
+        <h1>Welcome to Puremo!</h1>
         <p>This is a simple React app running inside a Docker container.</p>
         <button onClick={() => alert('Button clicked!')}>Click Me</button>
       </div>
