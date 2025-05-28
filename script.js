bb1f06812910cf23b50030f5c41d64a5a9b6c8dc
Hrithik-Upadhyay-AU46 <Hrithik-Upadhyay-AU46@users.noreply.github.com>
2025-05-28 04:14:04 +0000

    Development Build for PR #{pr_number}: (30d4425) Merge 43cf1e30f3a00072d68cb0ea08ba20cce3e93357 into 21f3bb6a45aa4d43958d699391b7d90c2ab0a60c

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('colorButton').addEventListener('click', () => {
-    const colors = ['rgb(240, 244, 248)', 'rgb(232, 248, 228)', 'rgb(248, 228, 228)', 'rgb(228, 248, 248)'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-});
